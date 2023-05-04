f22359a5e76b8c9e348daf7d874e32e2d69f574c
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-05-04 17:41:47 +0200

    feat like button click

--- main.js
+++ main.js
@@ -56,40 +56,52 @@ const posts = [
     }
 ];
 
-let mainContainer = document.getElementById("container")
+const mainContainer = document.getElementById("container")
 
 posts.forEach(element => {
     console.log (element.author.image)
     mainContainer.innerHTML +=     
-`<div class="post">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+        `<div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
+                </div>
             </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${element.author.name}</div>
-                <div class="post-meta__time">${element.created}</div>
-            </div>                    
-        </div>
-    </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-    <div class="post__image">
-        <img src="${element.media}" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
             </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
-            </div>
-        </div> 
-    </div>            
-</div>`
-    
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`
+
 });
+
+
+const likeButtons = document.querySelectorAll(".js-like-button")
+console.log (likeButtons)
+
+for (let index = 0; index < likeButtons.length; index++) {
+    const likeButton = likeButtons[index];
+    likeButton.addEventListener("click", function() {
+        this.classList.toggle("like-button--liked");
+    });
+    
+}
